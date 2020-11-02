897f39a44e1f4009f375f563a55dab2e62446872
jazmin <jazmincavanagh@hotmail.com>
2020-11-02 15:26:19 -0300

    commit

--- proyecto/database/config/config.js
+++ proyecto/database/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   "development": {
-    "username": "root",
-   "password": "root",
+   "username": "root",
+   "password": null,
     "database": "proyectodh",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port":8889,
-   // "port": 3306
+    //"port":8889,
+    "port": 3306
   },
   "test": {
     "username": "root",
