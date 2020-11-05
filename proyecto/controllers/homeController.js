1b34d9e4e5d54ad458e5abcaa1fb7c1d374e95eb
Lucia <lsalasgonzalez@udesa.edu.ar>
2020-11-05 16:48:41 -0300

    session

--- proyecto/controllers/homeController.js
+++ proyecto/controllers/homeController.js
@@ -48,7 +48,7 @@ let homeController = {
         storePost: function (req, res){
             
             let postear = {
-                usuario_id: req.body.usuario_id,
+                usuario_id: req.session.user.id,
                 url: req.body.url,
                 descripcion: req.body.descripcion,
                 ubicacion: req.body.ubicacion
